bbe8768e60b8e77b8042ba941a0f8aef7cb597b9
Mike Gallegos and Veronica Alley <student@epicodus.com>
2016-01-11 16:17:46 -0800

    add know card feature

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,96 @@
 $ (function(){
   $('.card').flip();
 
+
+  $("#yes1").click(function() {
+    $('#card1').fadeOut();
+    $('#yes1').fadeOut();
+  });
+
+  $("#yes2").click(function() {
+    $('#card2').fadeOut();
+    $('#yes2').fadeOut();
+  });
+
+  $("#yes3").click(function() {
+    $('#card3').fadeOut();
+    $('#yes3').fadeOut();
+  });
+
+  $("#yes4").click(function() {
+    $('#card4').fadeOut();
+    $('#yes4').fadeOut();
+  });
+
+  $("#yes5").click(function() {
+    $('#card5').fadeOut();
+    $('#yes5').fadeOut();
+  });
+
+  $("#yes6").click(function() {
+    $('#card6').fadeOut();
+    $('#yes6').fadeOut();
+  });
+
+  $("#yes7").click(function() {
+    $('#card7').fadeOut();
+    $('#yes7').fadeOut();
+  });
+
+  $("#yes8").click(function() {
+    $('#card8').fadeOut();
+    $('#yes8').fadeOut();
+  });
+
+  $("#yes9").click(function() {
+    $('#card9').fadeOut();
+    $('#yes9').fadeOut();
+  });
+
+  $("#yes10").click(function() {
+    $('#card10').fadeOut();
+    $('#yes10').fadeOut();
+  });
+
+  $("#yes11").click(function() {
+    $('#card11').fadeOut();
+    $('#yes11').fadeOut();
+  });
+
+  $("#yes12").click(function() {
+    $('#card12').fadeOut();
+    $('#yes12').fadeOut();
+  });
+
+  $("#yes13").click(function() {
+    $('#card13').fadeOut();
+    $('#yes13').fadeOut();
+  });
+
+  $("#yes14").click(function() {
+    $('#card14').fadeOut();
+    $('#yes14').fadeOut();
+  });
+
+  $("#yes15").click(function() {
+    $('#card15').fadeOut();
+    $('#yes15').fadeOut();
+  });
+
+  $("#yes16").click(function() {
+    $('#card16').fadeOut();
+    $('#yes16').fadeOut();
+  });
+
+  $("#yes17").click(function() {
+    $('#card17').fadeOut();
+    $('#yes17').fadeOut();
+  });
+
+  $("#yes18").click(function() {
+    $('#card18').fadeOut();
+    $('#yes18').fadeOut();
+  });
+
+
 });
